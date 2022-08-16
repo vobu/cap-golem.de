caa7f04aca1d96c69c3c5e56ecd4cfab1d88bf89
Volker Buzek <volker.buzek@js-soft.com>
2022-08-16 22:08:26 +0200

    refactor: besseres adjektiv

--- srv/shop-odata.js
+++ srv/shop-odata.js
@@ -1,9 +1,12 @@
 module.exports = (srv) => {
   // `srv` is the instance of cds.Service
-  srv.after("READ", "Breweries", (brewery) => {
-    if (brewery.length) {
-      for (let b of brewery) {
-        b.name = `lecker ${b.name}`
+  srv.after("READ", "Beers", (beers) => {
+    const adjectives = ["lecker", "wahnsinns", "mega", "bomben", "hammer"]
+    if (beers.length) {
+      for (let b of beers) {
+        const id = ~~(adjectives.length * Math.random())
+        b.name = `${adjectives[id]} ${b.name}`
+        adjectives.splice(id, 1)
       }
     }
   })
